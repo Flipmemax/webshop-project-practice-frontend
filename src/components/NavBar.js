aa3687328931a0415001b16ecd7b514d97aad216
Flipmemax <maxphoogland@gmail.com>
2021-05-29 11:08:30 +0200

    first commit ~~ basic setup react router, redux store, skeleton pages

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom";
+import "../App.css";
+
+export default function NavBar() {
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ margin: 20, marginTop: 5 }}>
+        <NavLink
+          to="/"
+          className="Applink"
+          activeStyle={{ color: "greenyellow" }}
+          exact
+        >
+          Home
+        </NavLink>
+      </div>
+      <div style={{ margin: 20, marginTop: 5 }}>
+        <NavLink
+          to="/signup"
+          className="Applink"
+          activeStyle={{ color: "greenyellow" }}
+        >
+          Sign Up
+        </NavLink>
+      </div>
+      <div style={{ margin: 20, marginTop: 5 }}>
+        <NavLink
+          to="/login"
+          className="Applink"
+          activeStyle={{ color: "greenyellow" }}
+        >
+          Login
+        </NavLink>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <div>
+      <h1>Sign Up Here!</h1>
+    </div>
+  );
+}
